7c7ef3c9d590973a09055e2a236c549616e24506
tsk05074 <gts1630@naver.com>
2023-05-08 22:23:21 +0900

    2. Swap

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
 using UnityEngine;
+using DG.Tweening;
 
 public class Board : MonoBehaviour
 {
@@ -13,6 +15,10 @@ public class Board : MonoBehaviour
 
     public int Width => Tiles.GetLength(0);
     public int Height => Tiles.GetLength(1);
+
+    private readonly List<Tile> _selection = new List<Tile>();
+
+    private const float TweenDuration = 0.25f;
 
     private void Awake() => Instance = this;
 
@@ -33,6 +39,48 @@ public class Board : MonoBehaviour
                 Tiles[x, y] = tile;
             }
         }
-            
     }
+
+    public async void Select(Tile tile){
+        if(!_selection.Contains(tile)) _selection.Add(tile);
+
+        if(_selection.Count < 2) return;
+
+        await Swap(_selection[0], _selection[1]);
+
+        _selection.Clear();
+    }
+
+    public async Task Swap(Tile tile1, Tile tile2){
+        var icon1 = tile1.icon;
+        var icon2 = tile2.icon;
+
+        var icon1Transform = icon1.transform;
+        var icon2Transform = icon2.transform;
+
+        var sequence = DOTween.Sequence();
+
+        sequence.Join(icon1Transform.DOMove(icon2Transform.position, TweenDuration))
+                .Join(icon2Transform.DOMove(icon1Transform.position, TweenDuration));
+
+        await sequence.Play()
+                      .AsyncWaitForCompletion();
+        
+        icon1Transform.SetParent(tile2.transform);
+        icon2Transform.SetParent(tile1.transform);
+
+        tile1.icon = icon2;
+        tile2.icon = icon1;
+
+        var tile1Item = tile1.Item;
+
+        tile1.Item = tile2.Item;
+        tile2.Item = tile1Item;
+    }
+
+    public void CanPop(){
+
+    }
+
+
 }
